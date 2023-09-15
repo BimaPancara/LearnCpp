de2731ba3218cd61b71f1a738cb58afe7cd210b8
BimaPancara <bimapancara0@gmail.com>
2023-09-15 20:14:42 +0700

    Add project files.

---
+++ Snippet_Code_to_write/Snippet_Code_to_write.cpp
@@ -0,0 +1,27 @@
+#include <iostream> //std library
+
+int main()
+{
+    //1.2 Single Line Comments [15/9/2023]
+    //Demonstrate the use of comments
+    std::cout << "Hello World\n";
+
+    // this is much easier to read
+    std::cout << "It is very nice to meet you!\n";
+
+    // don't you think so?
+    std::cout << "Yeah!\n";
+
+    //1.3 Multi Line Comments
+
+    /* This is a multi-line comment.
+   This line will be ignored.
+   So will this one. */
+
+    // Looks Ugly, how about :
+    /* This is a multi-line comment.
+     * the matching asterisks to the left
+     * can make this easier to read
+     */
+    return 0;
+}
